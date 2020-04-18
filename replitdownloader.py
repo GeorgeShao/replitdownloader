bbf56de1e5f1e15c622cea54f049e5ad7d268e16
George Shao <GeorgeShao123@gmail.com>
2020-04-18 16:15:30 -0400

    Added assignment webpage opening functionality

--- replitdownloader.py
+++ replitdownloader.py
@@ -9,20 +9,23 @@ except:
     print("ERROR: Please install selenium using the command 'pip install selenium'")
     sys.exit()
 
-classroom_id = str(input("Enter the classroom id: "))
+classroom_id = input("Enter your classroom id (6 digit number): ")
+num_assigments = input("Please enter the number of assignments: ")
 
 driver = webdriver.Chrome()
-driver.get(f"https://repl.it/login?goto=%2Fstudent%2Fclassrooms%2F{classroom_id}")
-
-# email_box = driver.find_element_by_name("username")
-# email_box.send_keys(email)
-
-# time.sleep(2)
+driver.get(f'https://repl.it/login?goto=%2Fstudent%2Fclassrooms%2F{classroom_id}'')
 
 print("Please login to repl.it through the popup window")
 
-logged_in = False
-
 while True:
-    if driver.current_url == "https://repl.it/student/classrooms/177470" and not logged_in:
+    if driver.current_url == f'https://repl.it/student/classrooms/{classroom_id}'':
         print("Successfully logged in!")
+        break
+
+print("Webpage is loading...")
+
+loaded_status = input("Type anything here once the webpage has finished loading: ")
+
+for i in range(1, num_assigments+1):
+    element = driver.find_element_by_xpath(f'//*[@id="classroomsPage"]/div[3]/div[3]/div/div[2]/div/div[2]/div[{i}]')
+    element.click()
