0d77d1d5a58a9ad4f5585cedf851e3efc75d867d
George Shao <GeorgeShao123@gmail.com>
2020-04-18 13:09:35 -0400

    Added post-login message

--- replitdownloader.py
+++ replitdownloader.py
@@ -1,7 +1,7 @@
 import sys
 import time
 
-print("replitdownloader - a tool for downloading all files from a repl.it classroom")
+print("Repl.it Downloader - a tool for downloading all files from a repl.it classroom")
 
 try:
     from selenium import webdriver
@@ -12,10 +12,17 @@ except:
 classroom_id = str(input("Enter the classroom id: "))
 
 driver = webdriver.Chrome()
-driver.get(f"https://repl.it/student/classrooms/{classroom_id}")
+driver.get(f"https://repl.it/login?goto=%2Fstudent%2Fclassrooms%2F{classroom_id}")
 
 # email_box = driver.find_element_by_name("username")
 # email_box.send_keys(email)
 
 # time.sleep(2)
 
+print("Please login to repl.it through the popup window")
+
+logged_in = False
+
+while True:
+    if driver.current_url == "https://repl.it/student/classrooms/177470" and not logged_in:
+        print("Successfully logged in!")
